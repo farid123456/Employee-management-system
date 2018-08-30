28867f0d05f648a2c93f20f3c3863d1983fc31ed
Muhammad Zubair <muhammad.zubair@mobilelive.ca>
2018-08-30 11:23:35 +0500

    Add Dependencies using Bower

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,11 @@ angular.module('EmployeeManagementSystem', ['ngRoute','pascalprecht.translate'])
 
         $translateProvider.translations('en', {
             TITLE: 'Employee Management System',
+            namePlaceholder: 'Enter Employee Name',
+            emailPlaceholder: 'Enter Employee Email',
+            phonePlaceholder: 'Enter Employee Phone',
+            searchPlaceholder: 'Search Employee by Name',
+            saveButton: 'Save',
             HEADER: 'EM System',
             c1: 'Click the Below link to view the employee record in the Organization.',
             c2: 'Some Important Features of this Application:',
@@ -24,6 +29,11 @@ angular.module('EmployeeManagementSystem', ['ngRoute','pascalprecht.translate'])
 
         $translateProvider.translations('fr', {
             TITLE: 'Employé La gestion Système ',
+            namePlaceholder: 'Entrer Employé Prénom',
+            emailPlaceholder: 'Entrer Employé Email',
+            phonePlaceholder: 'Entrer Employé Téléphone',
+            saveButton: 'Enregistrer',
+            searchPlaceholder: 'Chercher Employé par Prénom',
             HEADER: 'EM Système',
             c1: 'Cliquez sur le lien ci-dessous pour afficher le dossier des employés dans l\'organisation.',
             c2: 'Quelques caractéristiques importantes de cette Application:',
